be50a28d1430f590d70dd95284f78eb81ed26001
Nathan <long3000@gmail.com>
2019-07-05 17:20:16 -0700

    Init webserver

---
+++ server.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+const (
+	// Port to URL
+	Port = ":8080"
+)
+
+func serveDynamic(w http.ResponseWriter, r *http.Request) {
+	response := "Current time: " + time.Now().String()
+	fmt.Fprintln(w, response)
+}
+
+func serveStatic(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static.html")
+}
+
+func main() {
+	http.HandleFunc("/static", serveStatic)
+	http.HandleFunc("/", serveDynamic)
+	http.ListenAndServe(Port, nil)
+}
